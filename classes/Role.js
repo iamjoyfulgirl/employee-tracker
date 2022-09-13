a6157565b1f3f1f1770a73e60bcdc22d1a45803d
iamjoyfulgirl <sherri@nymeria.lan>
2022-09-12 17:46:41 -0700

    Adds content to Role class

---
+++ classes/Role.js
@@ -0,0 +1,9 @@
+class Role {
+    constructor(title, salary, department_id) {
+        this.title = title;
+        this.salary = salary;
+        this.department_id = department_id;
+    }
+}
+
+module.exports = Role;
