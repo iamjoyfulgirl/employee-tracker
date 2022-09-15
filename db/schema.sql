fa016fdb97b4bb32f29854dd68ba3dbda91b0c72
iamjoyfulgirl <sherri@nymeria.lan>
2022-09-15 12:54:46 -0700

    Update SQL to use ON DELETE CASCADE to resolve errors when deleting

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(11,2) NOT NULL,
 department_id INT NOT NULL,
-FOREIGN KEY (department_id) REFERENCES departments(id));
+FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE);
 
 CREATE TABLE employees(
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -20,5 +20,5 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
 manager_id INT,
-FOREIGN KEY (role_id) REFERENCES roles(id),
+FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
 FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL);
